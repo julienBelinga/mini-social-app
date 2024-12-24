7eccd10923caddc3f876acb2a62edfa90632e909
julien belinga <julienbelinga29@gmail.com>
2024-12-24 18:16:22 +0100

    add custom button to login page and implement tap functionality

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -2,15 +2,31 @@ import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
   final String text;
+  final void Function()? onTap;
 
   const CustomButton({
     super.key,
     required this.text,
+    required this.onTap,
   });
 
   @override
-  Widget build(Object context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        padding: EdgeInsets.all(25),
+        child: Center(
+          child: Text(
+            this.text,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mini_social_app/components/custom_button.dart';
 import 'package:mini_social_app/components/custom_textfield.dart';
 
 class Login extends StatelessWidget {
@@ -41,7 +42,28 @@ class Login extends StatelessWidget {
                 controller: passwordController,
               ),
 
+              SizedBox(height: 10),
+
+              // Password forgotten link TODO: convert the text into a link
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "forgot password ?",
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(height: 50),
+
               // Login Button
+              CustomButton(
+                text: "Login",
+                onTap: () {},
+              )
 
               // Register page redirection
             ],
